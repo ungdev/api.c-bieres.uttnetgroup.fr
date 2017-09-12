818ee1653cdfcd8be81afdd348caeca418ef28d6
prudywsh <antoineprudhomme5@gmail.com>
2017-09-12 09:32:52 +0200

    started to work on auth + env created (commit before try somthing)

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+const oauthRoutes = require('./routes/oauth');
+
+app.use(cors());
+
+app.use('/api/oauth', oauthRoutes);
 
 app.listen(3000, function () {
   console.log('Server listening on port 3000!');
