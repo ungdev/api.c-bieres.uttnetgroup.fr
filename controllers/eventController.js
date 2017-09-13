63f206ffc077d40c3233648d82ab0e8781afa844
prudywsh <antoineprudhomme5@gmail.com>
2017-09-13 21:53:55 +0200

    rest routes created for event

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,6 +1,22 @@
 const mongoose = require('mongoose');
 const Event = mongoose.model('Event');
 
+exports.get = function(req, res) {
+    Event.find({}, (err, event) => {
+        if (err)
+            res.send(err);
+        res.json(event);
+    });
+};
+
+exports.getById = function(req, res) {
+    Event.findById(req.params.id, (err, event) => {
+        if (err)
+            res.send(err);
+        res.json(event);
+    });
+};
+
 exports.create = function(req, res) {
     const newEvent = new Event(req.body);
     newEvent.save((err, event) => {
@@ -8,4 +24,20 @@ exports.create = function(req, res) {
             res.send(err);
         res.json(event);
     });
-}
+};
+
+exports.update = function(req, res) {
+    Event.findOneAndUpdate({_id: req.params.id}, req.body, {new: true}, (err, event) => {
+        if (err)
+            res.send(err);
+        res.json(event);
+    });
+};
+
+exports.delete = function(req, res) {
+    Event.remove({_id: req.params.id}, (err, event) => {
+        if (err)
+            res.send(err);
+        res.json({ message: "Évènement supprimé" });
+    });
+};
--- routes/routes.js
+++ routes/routes.js
@@ -3,12 +3,18 @@ const oauthController = require('../controllers/oauthController');
 
 module.exports = function(app) {
 
+    // event routes
     app.route('/api/event')
+        .get(eventController.get)
         .post(eventController.create);
+    app.route('/api/event/:id')
+        .get(eventController.getById)
+        .put(eventController.update)
+        .delete(eventController.delete);
 
+    // oauth routes
     app.route('/api/etuutt/link')
         .get(oauthController.getLink);
-
     app.route('/api/etuutt/callback')
         .post(oauthController.callback);
 };
