88bd8f5f5f32d244b239c9306031ecf98177fae0
prudywsh <antoineprudhomme5@gmail.com>
2018-03-26 13:14:25 +0200

    can use html template to send mail with nodemailer

--- helpers/emailHelper.js
+++ helpers/emailHelper.js
@@ -1,27 +1,41 @@
+const path = require('path')
 const nodemailer = require('nodemailer')
+const Email = require('email-templates');
 const mongoose = require('mongoose')
 const Drinker = mongoose.model('Drinker')
+
+const templatesDir = path.resolve(__dirname, '..', 'emails')
 
 const newEvent = () => {
   return Drinker.find({ newsletters: { $ne: false } })
     .then(drinkers => {
-      const socks = process.env.EMAIL_SOCKS
-      const username = process.env.EMAIL_USERNAME
-      const password = process.env.EMAIL_PASSWORD
-      const hostname = process.env.EMAIL_HOSTNAME
-      const port = process.env.EMAIL_PORT
-      let transporter = nodemailer.createTransport(`${socks}://${username}:${password}@${hostname}`)
 
-      const mailOptions = {
-        from: '"C bieres 👥" <antoineprudhomme5@gmail.com>',
-        to: 'antoineprudhomme5@gmail.com',
-        subject: 'Prochaine dégustation',
-        text: 'Hello world 🐴',
-        html: '<b>Hello world 🐴</b>'
-      }
+      const email = new Email()
 
-      return transporter.sendMail(mailOptions)
-    })
+      email
+      .renderAll('event', {
+        name: 'Elon'
+      })
+      .then(data => {
+        const socks = process.env.EMAIL_SOCKS
+        const username = process.env.EMAIL_USERNAME
+        const password = process.env.EMAIL_PASSWORD
+        const hostname = process.env.EMAIL_HOSTNAME
+        const port = process.env.EMAIL_PORT
+        let transport = nodemailer.createTransport(`${socks}://${username}:${password}@${hostname}`)
+
+        const mailOptions = {
+          from: '"C bieres 👥" <antoineprudhomme5@gmail.com>',
+          to: 'antoineprudhomme5@gmail.com',
+          subject: 'Prochaine dégustation',
+          text: data.text,
+          html: data.html
+        }
+        console.log(data.text)
+        console.log(data.html)
+        return transport.sendMail(mailOptions)
+   })
+ })
 }
 
 module.exports = { newEvent }
